22061dcf2829e64cb68783f29991fa4e639ec383
mcwilr <mcwilr@rpi.edu>
2019-05-09 15:03:15 -0400

    added user icon

--- Header.php
+++ Header.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <style>
     .header {
@@ -30,7 +31,7 @@
         if (!isset($_SESSION['token'])) {
             echo '<a class="topright" href="./index.php" style="text-decoration: none;">Login</a>';
         } else {
-            echo '<div class="topright">' . $_SESSION['username'] . ': <a href="./php/logoff.php" style="text-decoration: none;">Logoff</a></div>';
+            echo '<div class="topright"><i class="fas fa-user"></i> ' . $_SESSION['username'] . ': <a href="./php/logoff.php" style="text-decoration: none;">Logoff</a></div>';
         }
     ?>
   </div>
